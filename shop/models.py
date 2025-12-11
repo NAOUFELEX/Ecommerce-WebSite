6c2ba7cf55f8ba293903b25d8f838b6c1450ba04
NAOUFELEX <kellilnaoufel@gmail.com>
2025-12-10 16:54:08 -0800

    Create shop app and initial project files

---
+++ shop/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
